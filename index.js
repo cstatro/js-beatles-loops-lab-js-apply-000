2aa9c532d0b508a5ece91270d5daef8fdf471a9b
Colin Tatro <cstatro@gmail.com>
2019-05-01 00:50:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(bandMembers,instruments){
-  var beatlesArray = []
+  let beatlesArray = []
   for(var i = 0; i < bandMembers.length;i++){
     beatlesArray.push(`${bandMembers[i]} plays ${instruments[i]}`)
   }
