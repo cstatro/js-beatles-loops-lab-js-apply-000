a4ceb34d9e745e4df40dd046a37665c2c12b9c7a
Colin Tatro <cstatro@gmail.com>
2019-05-01 00:49:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // add solution here
 function theBeatlesPlay(bandMembers,instruments){
-  
+  beatlesArray = []
+  for(var i = 0; i < bandMembers.length;i++){
+    beatlesArray.push(`${bandMembers[i]} plays ${instruments[i]}`)
+  }
+  return beatlesArray
 }
